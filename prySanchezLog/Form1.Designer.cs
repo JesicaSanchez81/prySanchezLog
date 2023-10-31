ef6dd1d0012a983c1f41f37b68940cbd51baa476
root <mgiraudo@ies21.edu.ar>
2023-10-31 17:02:50 -0300

    Agregar archivos de proyecto.

---
+++ prySanchezLog/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace prySanchezLog
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblEstadoConexion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.dgvRegistro = new System.Windows.Forms.DataGridView();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblEstadoConexion});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "Barra inferior";
+            // 
+            // lblEstadoConexion
+            // 
+            this.lblEstadoConexion.Name = "lblEstadoConexion";
+            this.lblEstadoConexion.Size = new System.Drawing.Size(112, 17);
+            this.lblEstadoConexion.Text = "Estado de Conexion";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(88, 39);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(175, 48);
+            this.btnConectar.TabIndex = 1;
+            this.btnConectar.Text = "Conectar a Base de Datos";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // dgvRegistro
+            // 
+            this.dgvRegistro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRegistro.Location = new System.Drawing.Point(424, 49);
+            this.dgvRegistro.Name = "dgvRegistro";
+            this.dgvRegistro.Size = new System.Drawing.Size(240, 150);
+            this.dgvRegistro.TabIndex = 2;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(505, 229);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 3;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.dgvRegistro);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "frmPrincipal";
+            this.Text = "Principal";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistro)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblEstadoConexion;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.DataGridView dgvRegistro;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ prySanchezLog/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySanchezLog
+{
+    public partial class frmPrincipal : Form
+    {
+        clsAccesoBD objAccesoBD = new clsAccesoBD();    
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            objAccesoBD.ConectarBaseDatos();
+
+            lblEstadoConexion.Text = objAccesoBD.EstadoConexion;
+
+            //MessageBox.Show(objAccesoBD.EstadoConexion);
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            objAccesoBD.TraerDatosDataSet(dgvRegistro);
+
+            if (txtNombre.Text != "")
+            {
+                objAccesoBD.RegistrarDatosDataSet(txtNombre.Text);
+            }
+        }
+
+    }
+}
+}
